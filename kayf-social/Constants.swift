85ba0c179fd8dd12ccd64d1e3490e3d205a575b8
alex dev <alexdev@Melanies-MacBook-Pro.local>
2017-04-29 04:13:22 -0700

    Sign in ui custom classes complete

---
+++ kayf-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  kayf-social
+//
+//  Created by alex dev on 4/29/17.
+//  Copyright © 2017 alex dev. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
